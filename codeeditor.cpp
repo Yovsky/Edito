6da8d28b598cec4055df15d8846b05392ceeaf22
Imprimichyovsky <imprimichyovsky@proton.me>
2025-08-20 00:01:01 +0100

    Improved the layout, changed the font and Added the Tab spacing.

--- codeeditor.cpp
+++ codeeditor.cpp
@@ -5,6 +5,13 @@
 
 CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
 {
+    QFont f("Consolas"); //Setting the font parameters.
+    f.setStyleHint(QFont::Monospace);
+    setFont(f);
+
+    const int spaceWi = fontMetrics().horizontalAdvance(' '); //Adding the tab spacing.
+    setTabStopDistance(spaceWi * 4);
+
     lineNumberArea = new LineNumberArea(this);
 
     connect(this, &CodeEditor::blockCountChanged, this, &CodeEditor::updateLineNumberAreaWidth);
@@ -17,9 +24,9 @@ CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
 
 int CodeEditor::lineNumberAreaWidth()
 {
-    int digits = 1;
+    int digits = 3; //Set the initial margin width.
     int max = qMax(1, blockCount());
-    while (max >= 10) {
+    while (max >= 10) { //Adjust the width for bigger line counts.
         max /= 10;
         ++digits;
     }
@@ -51,7 +58,6 @@ void CodeEditor::resizeEvent(QResizeEvent *e)
     QRect cr = contentsRect();
     lineNumberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
 }
-
 void CodeEditor::highlightCurrentLine()
 {
     QList<QTextEdit::ExtraSelection> extraSelections;
@@ -59,7 +65,7 @@ void CodeEditor::highlightCurrentLine()
     if (!isReadOnly()) {
         QTextEdit::ExtraSelection selection;
 
-        QColor lineColor = QColor(Qt::yellow).lighter(160);
+        QColor lineColor = QColor(Qt::red).lighter(15);
 
         selection.format.setBackground(lineColor);
         selection.format.setProperty(QTextFormat::FullWidthSelection, true);
@@ -74,7 +80,7 @@ void CodeEditor::highlightCurrentLine()
 void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
 {
     QPainter painter(lineNumberArea);
-    painter.fillRect(event->rect(), Qt::lightGray);
+    painter.fillRect(event->rect(), QColor(Qt::black).lighter(40)); //Draw the margin (set the color).
 
     QTextBlock block = firstVisibleBlock();
     int blockNumber = block.blockNumber();
@@ -84,14 +90,17 @@ void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
     while (block.isValid() && top <= event->rect().bottom()) {
         if (block.isVisible() && bottom >= event->rect().top()) {
             QString number = QString::number(blockNumber + 1);
-            painter.setPen(Qt::black);
-            painter.drawText(0, top, lineNumberArea->width() - 5, fontMetrics().height(),
-                             Qt::AlignRight, number);
+            painter.setPen(Qt::gray); //Set the numbers parameters.
+            painter.drawText(2, top, lineNumberArea->width() - 7, fontMetrics().height(),
+                             Qt::AlignRight, number); //Draw the numbers.
         }
 
-        block = block.next();
+        block = block.next(); //The margin numbers logic.
         top = bottom;
         bottom = top + (int) blockBoundingRect(block).height();
         ++blockNumber;
     }
+
+    painter.setPen(QPen (Qt::red, 2)); //Set the line parameters.
+    painter.drawLine(lineNumberArea->width()-1, event->rect().top(), lineNumberArea->width()-1, event->rect().bottom()); //Draw the red margin line.
 }
